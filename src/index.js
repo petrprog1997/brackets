37dcc0976f39f6c5a2c91e1a7e4dea214f4e3c82
petrprog1997 <petrprog1997@mail.ru>
2019-10-01 00:12:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   // your solution
+
+    let stack = [];
+    
+    let array = str.split('');
+
+    for(let i = 0; i<array.length; i++ ){
+        
+        if(array[i] == '|'){
+            
+            array[i] = '0';
+        }
+    }
+    
+    let k = 0; 
+        
+        
+            
+    for(let i = 0 ; i < array.length; i++) {
+    
+        for(let j = 0; j < bracketsConfig.length; j++) {
+                
+            if(array[i] == bracketsConfig[j][0]){
+                    
+                stack.push(array[i]);
+                    
+                k++;
+            }
+                
+            else if(array[i] == bracketsConfig[j][1] && stack[k-1] == bracketsConfig[j][0]){
+            
+                stack.pop();
+        
+                k--;
+            }
+        }
+    }
+    
+    console.log('stack ' + stack);
+    
+    if(stack.length == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
--- test.js
+++ test.js
@@ -34,7 +34,7 @@ it('should check if brackets sequence is correct 6', () => {
   assert.equal(check('[]()', config2), true);
 });
 
-it('should check if brackets sequence is correct 7', () => {
+it('should check if brackets sequence is not correct 7', () => {
   assert.equal(check('[]()(', config2), false);
 });
 
